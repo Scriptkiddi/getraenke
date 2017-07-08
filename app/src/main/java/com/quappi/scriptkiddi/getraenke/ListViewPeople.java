8b0cd42c108b9f134f87c83ac28645dcd180d5af
Fritz Otlinghaus <fritz@otlinghaus.it>
2017-07-08 18:43:00 +0200

    working

--- app/src/main/java/com/quappi/scriptkiddi/getraenke/ListViewPeople.java
+++ app/src/main/java/com/quappi/scriptkiddi/getraenke/ListViewPeople.java
@@ -1,21 +1,53 @@
 package com.quappi.scriptkiddi.getraenke;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.quappi.scriptkiddi.getraenke.adapter.PeopleListViewAdapter;
 import com.quappi.scriptkiddi.getraenke.utils.Person;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ListViewPeople extends AppCompatActivity {
+public class ListViewPeople extends AppCompatActivity implements SearchView.OnQueryTextListener {
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private PeopleListViewAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<Person> people = new ArrayList<>();
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +63,47 @@ public class ListViewPeople extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
         // specify an adapter
-        ArrayList<Person> people = new ArrayList<>();
-        people.add(new Person("Michel", "weitbrecht"));
-        people.add(new Person("Fritz", "weitbrecht"));
-        people.add(new Person("Janne", "weitbrecht"));
-        people.add(new Person("Pi", "weitbrecht"));
-        people.add(new Person("Jonas", "weitbrecht"));
+
+        people.add(new Person("Michel", "Weitbrecht"));
+        people.add(new Person("Fritz", "Otlinghaus"));
+        people.add(new Person("Janne", "Heß"));
+        people.add(new Person("Markus", "Mroch"));
+        people.add(new Person("Jonas", "Auer"));
         mAdapter = new PeopleListViewAdapter(people);
         mRecyclerView.setAdapter(mAdapter);
 
+
     }
 
+    @Override
+    public boolean onQueryTextChange(String query) {
+        final List<Person> filteredModelList = filter(people, query);
+        mAdapter.replaceAll(filteredModelList);
+        mRecyclerView.scrollToPosition(0);
+        return true;
+    }
+
+    private static List<Person> filter(List<Person> models, String query) {
+        final String lowerCaseQuery = query.toLowerCase();
+
+        final List<Person> filteredModelList = new ArrayList<>();
+        for (Person model : models) {
+            final String text = model.getFirstName().toLowerCase();
+            if (text.contains(lowerCaseQuery)) {
+                filteredModelList.add(model);
+            }
+        }
+        return filteredModelList;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
 
 }
